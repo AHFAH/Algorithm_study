76c56bb76b59e7beb0831941bf8637559c276b48
AHFAH <jiyou626@gmail.com>
2026-03-16 23:44:02 +0900

    ADD: BFS 문제 추가

---
+++ problems/class3/_B2606.py
@@ -0,0 +1,30 @@
+import sys
+from collections import deque
+
+N = int(sys.stdin.readline())
+M = int(sys.stdin.readline())
+net = [[] for i in range(N+1)]
+
+for i in range(M):
+    a, b = map(int, sys.stdin.readline().split())
+    net[a].append(b)
+    net[b].append(a)
+
+
+def bfs():
+    q = deque()
+    count = 0
+    q.append(1)
+    visited[1] = True
+    while q:
+        cur = q.popleft()
+        for i, val in enumerate(net[cur]):
+            if visited[val] == False:
+                q.append(val)
+                visited[val] = True
+                count += 1
+
+    print(count)
+
+visited = [False for i in range(N+1)]
+bfs()
