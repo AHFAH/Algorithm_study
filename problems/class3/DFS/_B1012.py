ad672d807bf3165ef3c3fa14a784961814860984
AHFAH <jiyou626@gmail.com>
2026-03-21 23:45:15 +0900

    UPDATE: B1012 주석 추가

--- problems/class3/DFS/_B1012.py
+++ problems/class3/DFS/_B1012.py
@@ -3,7 +3,7 @@ from collections import deque
 
 T = int(sys.stdin.readline())
 
-dx = [-1, 1, 0, 0]
+dx = [-1, 1, 0, 0] 
 dy = [0, 0, -1, 1]
 
 def BFS(x, y):
@@ -13,14 +13,14 @@ def BFS(x, y):
     while q:
         x, y = q.pop(0)
 
-        for i in range(4):
-            nx = x + dx[i]
+        for i in range(4): # 탐색할 좌표를 변경하기 위한 x, y 좌표 설정
+            nx = x + dx[i]  
             ny = y + dy[i]
 
-            if not(0 <= nx < M and 0 <= ny < N):
+            if not(0 <= nx < M and 0 <= ny < N): # 탐색할 좌표가 행렬 밖에 존재할 경우 넘어감
                 continue
 
-            if graph[nx][ny] == 1:
+            if graph[nx][ny] == 1: # 탐색한 좌표가 1일 경우 0으로 변경 후 queue에 삽입
                 q.append((nx, ny))
                 graph[nx][ny] = 0
 
