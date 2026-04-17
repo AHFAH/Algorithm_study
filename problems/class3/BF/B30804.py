48c1e294b0157b2f31926e6b7601ea15b45a36b3
AHFAH <jiyou626@gmail.com>
2026-04-17 23:45:23 +0900

    ADD: 문제 추가

---
+++ problems/class3/BF/B30804.py
@@ -0,0 +1,20 @@
+N = int(input())
+
+fruit = list(map(int, input().split()))
+
+fruit_set = len(set(fruit))
+num = 0
+
+if fruit_set <= 2:
+    print(N)
+elif fruit_set == N:
+    print(2)
+else:
+    for a in range(N-1):
+        for b in range(N-1-a):
+            f = fruit[a:N-b]
+            if len(set(f)) <= 2:
+                num = max(num, len(f))
+    
+    print(num)
+
